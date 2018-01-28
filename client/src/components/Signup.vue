c9f8a1a46c3c6784729271ec108aa144a9098399
angrha <angrha@gmail.com>
2018-01-29 02:32:35 +0700

    fix signup

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,27 +1,46 @@
 <template>
-  <div class="latar">
-    <span><img src="https://www.californiapaints.com/wp-content/uploads/californiapaints-favicon.png" alt="E-lukis"></span>
+  <div class="bg-reg">
+    <span>
+      <img src="https://www.californiapaints.com/wp-content/uploads/californiapaints-favicon.png" alt="E-lukis">
+    </span>
     <form>
       <fieldset>
-        <div class="form-group">
-          <input v-model="formData.first_name" class="form-control" placeholder="Enter First name" type="text">
+        <div class="form-group row">
+          <!-- first_name -->
+          <label for="firstName" class="col-sm-3 col-form-label">First Name</label>
+          <div class="col-sm-9 lft">
+            <input v-model="formData.first_name" class="form-control" id="firstName" placeholder="Type Your First Name" type="text">
+          </div>
+          <!-- last_name -->
+          <label for="lastName" class="col-sm-3 col-form-label">Last Name</label>
+          <div class="col-sm-9 lft">
+            <input v-model="formData.last_name" class="form-control" id="lastName" placeholder="Type Your Last Name" type="text">
+          </div>
+          <!-- contact -->
+          <label for="staticContact" class="col-sm-3 col-form-label">Contact</label>
+          <div class="col-sm-9 lft">
+            <input v-model="formData.contact" class="form-control" id="staticContact" placeholder="Type Your Contact" type="text">
+          </div>
+          <!-- username -->
+          <label for="staticUsername" class="col-sm-3 col-form-label">Username</label>
+          <div class="col-sm-9 lft">
+            <input v-model="formData.username" class="form-control" id="staticUsername" placeholder="Type Your Username" type="text">
+          </div>
+          <!-- email -->
+          <label for="staticEmail" class="col-sm-3 col-form-label">Email</label>
+          <div class="col-sm-9 lft">
+            <input v-model="formData.email" class="form-control" id="staticEmail" placeholder="Type Your Email" type="email">
+          </div>
+          <!-- password -->
+          <label for="staticPass" class="col-sm-3 col-form-label">Password</label>
+          <div class="col-sm-9 lft">
+            <input v-model="formData.password" class="form-control" id="staticPass" placeholder="Type Your Password" type="password">
+          </div>
+          <!-- button sign up -->
+          <div class="col-sm-12">
+            <button @click="signup(formData)" type="button" class="btn btn-success btn-block spc">SIGN UP</button>
+          </div>
         </div>
-        <div class="form-group">
-          <input v-model="formData.last_name" class="form-control" placeholder="Enter Last name" type="text">
-        </div>
-        <div class="form-group">
-          <input v-model="formData.contact" class="form-control" placeholder="Enter Contact" type="text">
-        </div>
-        <div class="form-group">
-          <input v-model="formData.username" class="form-control" placeholder="Enter Username" type="text">
-        </div>
-        <div class="form-group">
-          <input v-model="formData.email" class="form-control" placeholder="Enter Email" type="email">
-        </div>
-        <div class="form-group">
-          <input v-model="formData.password" class="form-control" placeholder="Enter Password" type="password">
-        </div>
-        <button @click="signup(formData)" type="button" class="btn btn-success btn-block" >SIGN UP</button>
       </fieldset>
     </form>
 </div>
@@ -51,7 +70,7 @@ export default {
 </script>
 
 <style scoped>
-.latar {
+.bg-reg {
   background-color: rgba(238, 220, 220, 0.445);
   border-width: 3px;
   border-style: solid;
@@ -59,27 +78,36 @@ export default {
   border-radius: 10px;
   margin: auto;
   width: 35%;
-  color: white;
   padding: 0% 2% 2% 2%;
-  margin-top: 3%;
-}
-
-h2 {
-  margin: 0%;
-  padding: 0%;
-  color: black;
+  margin-top: 5%;
 }
 
 input {
-  width: 100%;
   margin: auto;
-  height: 30px;
-  padding: 1%;
+  padding: 1.5%;
 }
 
 img {
   width: 35%;
   padding: 0%;
   margin: 0%
+}
+
+label {
+  color: rgba(63, 38, 38, 0.973);
+  font-size: 120%;
+  padding-right: 0%;
+  text-align: left;
+  font-weight: bold;
+}
+
+.lft {
+  padding-left: 0%;
+}
+
+.spc {
+  font-size: 120%;
+  letter-spacing: 3px;
+  font-weight: bold;
 }
 </style>
