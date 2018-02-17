29a08e09b28a571e5c3ec64615d8b0666d64fe7c
angrha <angrha@gmail.com>
2018-02-17 13:04:00 +0700

    add middleware transaction

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -5,7 +5,7 @@ const isLogin = require('../helper/authentication')
 const isAdmin = require('../helper/authorization')
 const isSelf  = require('../helper/authOwnOrAdm')
 
-router.get('/',isLogin, isAdmin Transaction.findAll)
+router.get('/',isLogin, isAdmin, Transaction.findAll)
 router.post('/', isLogin, Transaction.create)
 
 module.exports = router;
